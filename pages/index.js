03f24d0ce66d41eca862c374695482a4a854d81d
EazyPau <poyizhi@gmail.com>
2022-07-19 22:02:48 +0800

    feat/create select box

--- pages/index.js
+++ pages/index.js
@@ -5,28 +5,37 @@ import { NavBar } from "../components/global/NavBar";
 import { Pagination } from "../components/data_display/Pagination";
 import { Table } from "../components/data_display/Table";
 import { Button } from "../components/global/Button";
-import { TextInput } from "../components/forms/TextInput";
+import { TextInput } from "../components/forms/InputField";
 import { Sidebar } from "../components/forms/Sidebar";
+import { SelectBox } from "../components/forms/SelectBox";
 // import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
 		<div className="bg-slate-200 min-h-screen flex flex-col justify-between">
 			<div>
-			  <NavBar />
+				<NavBar />
 				<div className="pt-10 px-3 w-11/12 xl:w-3/4 2xl:w-7/12 mx-auto">
+					<p>Table</p>
 					<Table />
 					<div className="pt-10">
+						<p>Pagination</p>
 						<Pagination />
 					</div>
 					<div className="pt-10">
+						<p>Button</p>
 						<Button />
 					</div>
-          <div className="pt-10">
+					<div className="pt-10">
+						<p>Text Input</p>
 						<TextInput />
 					</div>
-          <div className="pt-10">
+					<div className="pt-10">
 						<Sidebar />
+					</div>
+					<div className="pt-10">
+						<p>Select Box</p>
+						<SelectBox />
 					</div>
 				</div>
 			</div>
