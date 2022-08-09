c265cd042f8041c19fc42d7c0248f846aec12916
EazyPau <poyizhi@gmail.com>
2022-08-09 21:57:09 +0800

    feature: replace table header and rows with props and create customizable button component

--- components/global/Button.js
+++ components/global/Button.js
@@ -1,12 +1,43 @@
+import clsx from "clsx";
 import React from "react";
+import PropTypes from "prop-types";
 
-export const Button = () => {
+export const Button = ({
+	variant = "primary",
+	customColor,
+	customBorder,
+	customWidth,
+	onClick,
+	children,
+}) => {
+	const baseClassName = "inline-block text-sm font-medium focus:outline-none focus:ring";
+	const width = "w-auto px-12 py-3";
+	const border = "border border-indigo-600 rounded";
+	const variants = {
+		primary:
+			"bg-indigo-600 text-white active:text-indigo-500 hover:bg-transparent hover:text-indigo-600",
+		secondary: "",
+		inline: "",
+	};
+
+	const style = clsx(
+		customWidth || width,
+		baseClassName,
+		customBorder || border,
+		customColor || variants[variant]
+	);
+
 	return (
-		<a
-			className="inline-block px-12 py-3 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring"
-			href="#"
-		>
-			Download
-		</a>
+		<button className={style} onClick={onClick}>
+			{children}
+		</button>
 	);
 };
+
+Button.prototype= {
+	variant: PropTypes.string,
+	customBorder: PropTypes.string,
+	customColor: PropTypes.string,
+	customWidth: PropTypes.string,
+	onClick: PropTypes.func,
+}
