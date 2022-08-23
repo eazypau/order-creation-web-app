7ff72a54ac06a0ecdb98223fe692d6a033115119
Po YZ <poyizhi@gmail.com>
2022-08-23 17:41:01 +0800

    wip: seeding db

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,8 @@ const App: AppType = ({ Component, pageProps }) => {
 
 export default withTRPC<ServerRouter>({
     config({ ctx }) {
-        const url = process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL}/api/trpc`
+        const url = process.env.HEROKU_URL
+            ? `https://${process.env.HEROKU_URL}/api/trpc`
             : "http://localhost:3000/api/trpc";
 
         return { url };
